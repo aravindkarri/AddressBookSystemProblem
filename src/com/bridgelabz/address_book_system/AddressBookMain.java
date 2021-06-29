87d28987d984052073fa231a9c2099ffa017d7c0
aravindkarri <aravindkarri98495@gmail.com>
2021-06-29 20:22:27 +0530

    ADDED: Code to display welcome to address book program

---
+++ src/com/bridgelabz/address_book_system/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.address_book_system;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Address book program");
+
+	}
+
+}
